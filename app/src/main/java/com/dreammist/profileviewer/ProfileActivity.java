d5f89efb6a1551acaa909366e96c0c7951198688
kevinthomas <kthomas1208@gmail.com>
2017-03-14 21:23:10 -0400

    Added tabbed navigation with toasts

--- app/src/main/java/com/dreammist/profileviewer/ProfileActivity.java
+++ app/src/main/java/com/dreammist/profileviewer/ProfileActivity.java
@@ -1,11 +1,14 @@
 package com.dreammist.profileviewer;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.facebook.Profile;
@@ -16,12 +19,34 @@ import java.util.ArrayList;
 public class ProfileActivity extends AppCompatActivity {
     final String LOG_TAG = ProfileActivity.class.getName();
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_posts:
+                    Toast.makeText(ProfileActivity.this, "Posts", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.navigation_photos:
+                    Toast.makeText(ProfileActivity.this, "Photos", Toast.LENGTH_SHORT).show();
+                    return true;
+
+            }
+            return false;
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         ProfilePictureView profilePictureView = (ProfilePictureView) findViewById(R.id.profile_pic);
         Profile profile = Profile.getCurrentProfile();
