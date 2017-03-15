5aca2cdf0c53463f6debf124d78f33b6a2c994bd
kevinthomas <kthomas1208@gmail.com>
2017-03-15 01:24:48 -0400

    Created fragments for posts and photos

---
+++ app/src/main/java/com/dreammist/profileviewer/PostsFragment.java
@@ -0,0 +1,99 @@
+package com.dreammist.profileviewer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.facebook.Profile;
+import com.facebook.login.widget.ProfilePictureView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PostsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PostsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PostsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PostsFragment newInstance(String param1, String param2) {
+        PostsFragment fragment = new PostsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_posts, container, false);
+
+        ProfilePictureView profilePictureView = (ProfilePictureView)
+                rootView.findViewById(R.id.profile_pic);
+        Profile profile = Profile.getCurrentProfile();
+
+        if(profile == null) Toast.makeText(rootView.getContext(),
+                "Please login, first", Toast.LENGTH_SHORT).show();
+        else profilePictureView.setProfileId(profile.getId());
+
+
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("Hello, Status 1");
+        values.add("Hey there, Status 2");
+        values.add(("Hi! Status 3"));
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(values);
+        RecyclerView recyclerView = (RecyclerView)rootView.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+
+        return rootView;
+    }
+
+}
--- app/src/main/java/com/dreammist/profileviewer/ProfileActivity.java
+++ app/src/main/java/com/dreammist/profileviewer/ProfileActivity.java
@@ -3,37 +3,30 @@ package com.dreammist.profileviewer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.facebook.Profile;
-import com.facebook.login.widget.ProfilePictureView;
 
-import java.util.ArrayList;
+import static com.dreammist.profileviewer.R.id.container;
 
 public class ProfileActivity extends AppCompatActivity {
     final String LOG_TAG = ProfileActivity.class.getName();
+
+    private static final String SELECTED_ITEM = "selected_item";
+    private int selectedItem;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_posts:
-                    Toast.makeText(ProfileActivity.this, "Posts", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.navigation_photos:
-                    Toast.makeText(ProfileActivity.this, "Photos", Toast.LENGTH_SHORT).show();
-                    return true;
-
-            }
-            return false;
+            selectFragment(item);
+            return true;
         }
 
     };
@@ -45,35 +38,54 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        ProfilePictureView profilePictureView = (ProfilePictureView) findViewById(R.id.profile_pic);
         Profile profile = Profile.getCurrentProfile();
 
         if(profile == null) {
             Toast.makeText(this, "Please login, first", Toast.LENGTH_SHORT).show();
-
         }
         else {
-            profilePictureView.setProfileId(profile.getId());
             String fullName = profile.getFirstName() + " " + profile.getLastName();
             getSupportActionBar().setTitle(fullName);
-            Log.v(LOG_TAG, fullName + " " + profile.getId());
-
         }
 
-        ArrayList<String> values = new ArrayList<String>();
-        values.add("Hello, Status 1");
-        values.add("Hey there, Status 2");
-        values.add(("Hi! Status 3"));
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(values);
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(adapter);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(llm);
+        // Set the fragment when starting up the activity
+        MenuItem item;
+        if (savedInstanceState != null) {
+            selectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            item = navigation.getMenu().findItem(selectedItem);
+        } else {
+            item = navigation.getMenu().getItem(0);
+        }
+        selectFragment(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, selectedItem);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        switch (item.getItemId()) {
+            case R.id.navigation_posts:
+                frag = PostsFragment.newInstance("", "");
+                break;
+            case R.id.navigation_photos:
+                frag = PhotosFragment.newInstance("", "");
+                break;
+        }
+
+        // update selected item
+        selectedItem = item.getItemId();
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(container, frag, frag.getTag());
+            ft.commit();
+        }
     }
 }
